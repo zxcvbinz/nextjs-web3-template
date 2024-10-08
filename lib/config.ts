830d43186cb4bef6610ca8d20970657ace4e9187
zxcvbinz <davide.lanotte.unix@gmail.com>
2024-10-08 20:55:48 +0200

    feat: Add utility function for merging CSS classes
    - Added a new utility function `cn` in `lib/utils.ts` that merges multiple CSS classes using `clsx` and `twMerge` libraries.
    - This function will be useful for dynamically generating CSS classes in the application.

---
+++ lib/config.ts
@@ -0,0 +1,20 @@
+'use client';
+
+import { http, createStorage, cookieStorage } from 'wagmi';
+import { sepolia, bscTestnet, blastSepolia } from 'wagmi/chains';
+import { Chain, getDefaultConfig } from '@rainbow-me/rainbowkit';
+
+const projectId = '96ecf792e3cb4b95a3b20ba5fa4b9e57';
+
+const supportedChains: Chain[] = [sepolia, bscTestnet, blastSepolia];
+
+export const config = getDefaultConfig({
+	appName: 'WalletConnection',
+	projectId,
+	chains: supportedChains as any,
+	ssr: true,
+	storage: createStorage({
+		storage: cookieStorage
+	}),
+	transports: supportedChains.reduce((obj, chain) => ({ ...obj, [chain.id]: http() }), {})
+});
