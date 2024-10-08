7d6b2c551ee66a8e9c3b5e55c446fd0ac84b064f
zxcvbinz <davide.lanotte.unix@gmail.com>
2024-10-08 22:22:55 +0200

    Update hardhat.config.js and tsconfig.json

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,8 @@ module.exports = {
 	solidity: '0.8.27',
 	paths: {
 		sources: './hardhat/contracts',
-		tests: './hardhat/test'
+		tests: './hardhat/test',
+		cache: './hardhat/cache',
+		artifacts: './hardhat/artifacts'
 	}
 };
